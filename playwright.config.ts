5cff55cfa8e915859614cf86138df7b6d8f0e3cb
carlnoval <cjrnoval22@gmail.com>
2024-07-25 23:39:15 +0900

    Update: Use desired playwright configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,11 +24,22 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
+
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://the-internet.herokuapp.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    
+    /**
+     * Advantage: Full page screenshot on general failed tests.
+     * Disacvantage: Takes another unecessary screenshot for visual assertions, visual assertions already have expected vs actual screenshots. 
+     */
+    screenshot: {
+      mode: 'only-on-failure',
+      fullPage: true,
+    }
   },
 
   /* Configure projects for major browsers */
